c52dedf9a1a483ff4ac1ec329989da2b1fefd664
Qustio <qustioof@gmail.com>
2025-06-29 14:42:02 +0300

    Use @Data annotation on User

--- 2_Hibernate/src/main/java/org/example/entities/User.java
+++ 2_Hibernate/src/main/java/org/example/entities/User.java
@@ -1,47 +1,20 @@
 package org.example.entities;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
 
 import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "Users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private long id;
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public OffsetDateTime getCreatedAt() {
-        return created_at;
-    }
 
     @Column
     private String name;
@@ -64,16 +37,5 @@ public class User {
         this.email = email;
         this.age = age;
         this.created_at = created_at;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", age=" + age +
-                ", created_at=" + created_at +
-                '}';
     }
 }
